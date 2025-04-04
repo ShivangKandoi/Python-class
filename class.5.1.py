8072b2bb313255740f7523b08f8afb84e6ba63e0
Zemon-tech <shivangkandoi@Shivangs-MacBook-Air.local>
2025-04-04 14:16:57 +0530

    did soem corrections

--- class.5.1.py
+++ class.5.1.py
@@ -1,12 +1,25 @@
-def print_prime_number(n):
-    for num in range(2, n + 1):
-        is_prime = True
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
-                is_prime = False
-                break
-        if is_prime:
-            print(num)
-n = int(input("Enter a number: "))
-print("Prime numbers up to", n, "are:")
-print_prime_number(n)
+# Function to check if a number is prime
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+# Function to print the first n prime numbers
+def print_n_primes(n):
+    count = 0
+    num = 2
+    while count < n:
+        if is_prime(num):
+            print(num, end=' ')
+            count += 1
+        num += 1
+
+# Input from the user
+n = int(input("Enter the number of prime numbers to print: "))
+
+# Print the first n prime numbers
+print(f"The first {n} prime numbers are:")
+print_n_primes(n)
